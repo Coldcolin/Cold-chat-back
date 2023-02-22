9e6e57985417763d558665f5c3a40a4d52e9cc90
Coldcolin <decorcecolin@gmail.com>
2023-02-22 11:15:20 +0100

    changed some params

--- index.js
+++ index.js
@@ -20,6 +20,9 @@ mongoose.connect(URL, {useNewUrlParser: true, useUnifiedTopology: true}).then(()
     console.log(err.message)
 })
 
+
+
+
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
@@ -35,35 +38,67 @@ const io = socket(server, {
   });
 
   // global.onlineUsers = new Map();
-  global.messages = new Map();
+  // global.messages = new Map();
+
+  const db = mongoose.connection;
+
+  db.on("open", ()=>{
+    const observer = db.collection("chat-users").watch();
+    observer.on("change", (change)=>{
+      if(change.operationType === 'insert'){
+        const userData = {
+          username: change.fullDocument.username,
+          _id: change.fullDocument._id,
+          email: change.fullDocument.email 
+        }
+        io.emit("newUser", userData)
+      }
+    })
+    const messageObserver = db.collection("messages").watch();
+    messageObserver.on("change", (change)=>{
+      if(change.operationType === 'insert'){
+        const messageData ={
+          sender: change.fullDocument.sender.toString(),
+          message: change.fullDocument.message.text,
+        }
+        io.emit("recieve-message", messageData)
+        // console.log(messageData)
+      }
+    })
+  })
 
   io.on("connection", (socket) => {
-    global.chatSocket = socket;
-    socket.on("add-user", (userId) => {
-      messages.set(userId, socket.id);
+
+      console.log('connected', socket.id)
+      socket.on("disconnect", ()=>{
+        console.log("disconected")
+      })
+    // global.chatSocket = socket;
+    // socket.on("add-user", (userId) => {
+      // messages.set(userId, socket.id);
       // console.log(messages)
-    });
+    // });
   
-    socket.on("send-msg", (data) => {
-      const sendUserSocket = messages.get(data.to);
-      if (sendUserSocket) {
-        socket.to(sendUserSocket).emit("msg-recieve", data.msg);
-        // console.log(sendUserSocket)
-      }
-    });
+    // socket.on("send-msg", (data) => {
+    //   // const sendUserSocket = messages.get(data.to);
+    //   if (sendUserSocket) {
+    //     socket.to(sendUserSocket).emit("msg-recieve", data.msg);
+    //     // console.log(sendUserSocket)
+    //   }
+    // });
 
-    socket.on("new-user", (data)=>{
-      onlineUsers.set("newUser", data)
-    })
+    // socket.on("new-user", (data)=>{
+    //   onlineUsers.set("newUser", data)
+    // })
 
-    socket.on("get-users", (data)=>{
-      const allUsers = onlineUsers.get("newUser");
-      if(allUsers){
-        socket.to(allUsers).emit("users-recieve", data)
-      }
-      // io.emit("users", ()=>{
+    // socket.on("get-users", (data)=>{
+    //   const allUsers = onlineUsers.get("newUser");
+    //   if(allUsers){
+    //     socket.to(allUsers).emit("users-recieve", data)
+    //   }
+    //   // io.emit("users", ()=>{
         
-      // })
-    })
+    //   // })
+    // })
     
   });
